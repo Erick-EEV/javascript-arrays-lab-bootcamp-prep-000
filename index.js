334ef151563d7220a2a2a77ebd420018466bd044
Erick Villeda <eeverickeev@gmail.com>
2020-07-26 02:08:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function removeLastKitten(){
   return  kittens.slice(0, kittens.length - 4);
 }
 function removeFirstKitten(){
-  return array.slice(-2)
+  return kittens.slice(-2)
 }
 // Add your functions and code here
