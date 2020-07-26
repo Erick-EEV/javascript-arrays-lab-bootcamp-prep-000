dee0845b747849dd573141b2a40ee918799a9fc4
Erick Villeda <eeverickeev@gmail.com>
2020-07-26 01:24:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(name){
+
+}
+function destructivelyRemoveFirstKitten(){
+  
+}
+function append(name){
+  
+}
+function prependKitten(){
+  
+}
+function removeLastKitten(){
+  
+}
+function removeFirstKitten(){
+  
+}
 // Add your functions and code here
