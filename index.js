e96d511dbb2408c0b36a4ca8395783dcb6156645
Erick Villeda <eeverickeev@gmail.com>
2020-07-26 01:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyRemoveFirstKitten(){
 return kittens.shift();
 }
 function append(name){
-  return kittens.slice(appendkittens(name));
+  return kittens.concat(appendkittens(name));
 }
 function prependKitten(){
   
